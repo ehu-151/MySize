9ba62e4eb6f31bfe67274267f377648cdae08308
ehu <fehuehuf@gmail.com>
2017-12-29 16:09:09 +0900

    radioボタンの実装

--- app/src/main/java/com/example/ehu/mysize/HeightActivity.java
+++ app/src/main/java/com/example/ehu/mysize/HeightActivity.java
@@ -1,14 +1,19 @@
 package com.example.ehu.mysize;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class HeightActivity extends AppCompatActivity {
-
+    public static final String HEIGHT = "HEIGHT";
     private TextView mHeight;
 
     @Override
@@ -18,13 +23,17 @@ public class HeightActivity extends AppCompatActivity {
 
         mHeight = (TextView) findViewById(R.id.height);
 
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        int height = pref.getInt("HEIGHT", 160);
+        mHeight.setText(String.valueOf(height));
+
         Spinner spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Spinner spinner=(Spinner)parent;
-                String item =(String)spinner.getSelectedItem();
-                if(!item.isEmpty()){
+                Spinner spinner = (Spinner) parent;
+                String item = (String) spinner.getSelectedItem();
+                if (!item.isEmpty()) {
                     mHeight.setText(item);
                 }
             }
@@ -34,5 +43,45 @@ public class HeightActivity extends AppCompatActivity {
 
             }
         });
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setProgress(height);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                String value = String.valueOf(progress);
+                mHeight.setText(value);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        RadioGroup radio = findViewById(R.id.radioGroup);
+        radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                String value = radioButton.getText().toString();
+                mHeight.setText(value);
+            }
+        });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("HEIGHT", Integer.parseInt(mHeight.getText().toString()));
+        editor.commit();
+    }
+
 }
